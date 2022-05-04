0f233ef713efc8d3778a3ed06de2653886463c2b
naivexcited <optimusZHANG@hotmail.com>
2022-05-04 19:23:41 +0800

    切换字体功能基本完成

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,39 @@
-function changefontSONG() {
-    document.body.querySelectorAll("p").forEach((e) => {
+function changefont_BiaoTi_SONG() {
+    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
         e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
+        e.style.fontWeight = "700";
     });
 }
-function changefontHEI() {
+function changefont_BiaoTi_HEI() {
+    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
+        e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
+        e.style.fontWeight = "500";
+    });
+}
+
+function changefont_ZhengWen_SONG() {
+    document.body.querySelectorAll("p").forEach((e) => {
+        e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
+        e.style.fontWeight = "600";
+    });
+}
+function changefont_ZhengWen_HEI() {
     document.body.querySelectorAll("p").forEach((e) => {
         e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
+        e.style.fontWeight = "400";
+    });
+}
+
+function changefont_BianZhu_SONG() {
+    document.body.querySelectorAll(".sideNote").forEach((e) => {
+        e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
+        e.style.fontWeight = "600";
+    });
+}
+
+function changefont_BianZhu_HEI() {
+    document.body.querySelectorAll(".sideNote").forEach((e) => {
+        e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
+        e.style.fontWeight = "400";
     });
 }
