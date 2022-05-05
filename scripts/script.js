381cfa1226d9a651f0d804c8b8abdfe7af5dcd04
naivexcited <optimusZHANG@hotmail.com>
2022-05-05 12:02:06 +0800

    修改了换字体模块的写法

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,41 @@
-function changeTypeMenu_switchColour(active, inactive){
+function changeTypeMenu_switchColour(active, inactive) {
     var a = document.querySelector(active);
-    a.style.backgroundColor = "var(--colour-changeType-Active)";
+    a.style.cssText = "background-color: var(--colour-changeType-active) !important;";
     var b = document.querySelector(inactive);
     b.style.backgroundColor = "var(--colour-paper)";
-}
 
-function changefont_BiaoTi_SONG() {
-    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
-        e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
-        e.style.fontWeight = "700";
-    });
-    changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-SONG","#changeTypeMenu-BiaoTi-HEI");
 
 }
-function changefont_BiaoTi_HEI() {
-    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
-        e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
-        e.style.fontWeight = "500";
-    });
-    changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-HEI","#changeTypeMenu-BiaoTi-SONG");
 
-}
+// function changefont_BiaoTi_SONG() {
+//     document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
+//         e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
+//         e.style.fontWeight = "700";
+//     });
+//     changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-SONG", "#changeTypeMenu-BiaoTi-HEI");
+// }
+// function changefont_BiaoTi_HEI() {
+//     document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
+//         e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
+//         e.style.fontWeight = "500";
+//     });
+//     changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-HEI", "#changeTypeMenu-BiaoTi-SONG");
+// }
 
 function changefont_ZhengWen_SONG() {
     document.body.querySelectorAll("p").forEach((e) => {
         e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
         e.style.fontWeight = "600";
     });
-    changeTypeMenu_switchColour("#changeTypeMenu-ZhengWen-SONG","#changeTypeMenu-ZhengWen-HEI");
-    
+    changeTypeMenu_switchColour("#changeTypeMenu-ZhengWen-SONG", "#changeTypeMenu-ZhengWen-HEI");
+
 }
 function changefont_ZhengWen_HEI() {
     document.body.querySelectorAll("p").forEach((e) => {
         e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
         e.style.fontWeight = "400";
     });
-    changeTypeMenu_switchColour("#changeTypeMenu-ZhengWen-HEI","#changeTypeMenu-ZhengWen-SONG");
+    changeTypeMenu_switchColour("#changeTypeMenu-ZhengWen-HEI", "#changeTypeMenu-ZhengWen-SONG");
 }
 
 function changefont_BianZhu_SONG() {
@@ -43,7 +43,7 @@ function changefont_BianZhu_SONG() {
         e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
         e.style.fontWeight = "600";
     });
-    changeTypeMenu_switchColour("#changeTypeMenu-BianZhu-SONG","#changeTypeMenu-BianZhu-HEI");
+    changeTypeMenu_switchColour("#changeTypeMenu-BianZhu-SONG", "#changeTypeMenu-BianZhu-HEI");
 }
 
 function changefont_BianZhu_HEI() {
@@ -51,5 +51,21 @@ function changefont_BianZhu_HEI() {
         e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
         e.style.fontWeight = "400";
     });
-    changeTypeMenu_switchColour("#changeTypeMenu-BianZhu-HEI","#changeTypeMenu-BianZhu-SONG");
+    changeTypeMenu_switchColour("#changeTypeMenu-BianZhu-HEI", "#changeTypeMenu-BianZhu-SONG");
+}
+
+document.getElementById('changeTypeMenu-BiaoTi-SONG').onclick = function changefont_BiaoTi_SONG() {
+    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
+        e.style.fontFamily = "SONG" + "," + "Noto Serif SC";
+        e.style.fontWeight = "700";
+    });
+    changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-SONG", "#changeTypeMenu-BiaoTi-HEI");
+}
+
+document.getElementById('changeTypeMenu-BiaoTi-HEI').onclick = function changefont_BiaoTi_HEI() {
+    document.body.querySelectorAll("h1,h2,h3").forEach((e) => {
+        e.style.fontFamily = "HEI" + "," + "Noto Sans SC";
+        e.style.fontWeight = "500";
+    });
+    changeTypeMenu_switchColour("#changeTypeMenu-BiaoTi-HEI", "#changeTypeMenu-BiaoTi-SONG");
 }
