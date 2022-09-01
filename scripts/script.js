32b2a89e8f2a474bef78864eb9ee98c78bdc7317
naivexcited <optimusZHANG@hotmail.com>
2022-09-01 22:37:08 +0800

    更新版权信息内容

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+// Copyright (c) [2022] [Zhang Qingtian (张擎天)]
+// [Utypography\scripts\script.js] is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//        http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+
+
 // 初始的字体选择高亮
 function changeTypeMenu_initialActive() {
     let initial = ['changeTypeMenu-BiaoTi-SONG', 'changeTypeMenu-ZhengWen-HEI', 'changeTypeMenu-BianZhu-HEI'];
