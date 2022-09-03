9f390594ff5caf64de69c62bb933f9c541f7a464
Zhang Qingtian <im_zhangqingtian@163.com>
2022-09-04 01:16:21 +0800

    更新css和js文件的名称和授权信息

--- scripts/additional/u-Typography-Typography_of_this_Site-additional.js
+++ scripts/additional/u-Typography-Typography_of_this_Site-additional.js
@@ -1,3 +1,14 @@
+// Copyright (c) [2022] [Zhang Qingtian (张擎天) | u-Typography.site]
+// [u-Typography-*-additional.js] is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//        http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+
 let gridButton = document.getElementById("pageTypography-Button");
 
 gridButton.onclick = function () {
